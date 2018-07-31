0793658e4288e056301ed0dd578a681dc56c1918
julskhana <juls.tier5@gmail.com>
2018-07-31 13:26:07 -0500

    cambios proveedor

--- ProyectoJuBa/src/Objetos/proveedor.java
+++ ProyectoJuBa/src/Objetos/proveedor.java
@@ -6,5 +6,12 @@ import java.io.Serializable;
  * @author Jerson Junqui
  */
 public class proveedor implements Serializable{
-    
+   
+   private int id;
+
+    public proveedor(int id) {
+        this.id = id;
+    }
+   
+   
 }
